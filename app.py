9bab55e099876e1f990cf0dd9babc2d77370b0ee
Hakari Bibani <hakary.jalal33@gmail.com>
2024-11-06 23:04:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import time
-from PIL import Image
 import numpy as np
 from math import pi, sin, cos
 
@@ -16,16 +15,18 @@ def app():
     col1, col2 = st.columns(2)
     with col1:
         # Display the beaker
-        beaker = Image.open("beaker.png")
-        st.image(beaker, width=200, caption="CH3COOH")
+        st.markdown("""
+        <div style='width: 200px; height: 300px; border: 1px solid black; position: relative;'>
+        <div style='width: 100px; height: 150px; background-color: #FFD1DC; position: absolute; top: 50px; left: 50px;'>CH3COOH</div>
+        </div>
+        """, unsafe_allow_html=True)
 
     with col2:
         # Display the spoon and NaHCO3 text
-        spoon = Image.open("spoon.png")
-        st.image(spoon, width=100)
         st.markdown("""
-        <div style="display: flex; align-items: center;">
-        <div style="margin-right: 10px;">NaHCO₃</div>
+        <div style='display: flex; align-items: center;'>
+        <div style='width: 100px; height: 50px;'></div>
+        <div>NaHCO₃</div>
         </div>
         """, unsafe_allow_html=True)
 
@@ -35,7 +36,7 @@ def app():
         for i in range(50):
             spoon_x = 100 + 50 * sin(i * pi / 50)
             spoon_y = 200 + 50 * cos(i * pi / 50)
-            st.image(spoon, width=100, x_offset=int(spoon_x), y_offset=int(spoon_y))
+            st.markdown(f"<div style='width: 100px; height: 50px; position: absolute; left: {int(spoon_x)}px; top: {int(spoon_y)}px;'></div>", unsafe_allow_html=True)
             time.sleep(0.02)
         
         # Simulate the reaction
