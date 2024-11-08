a4f377159862cca3d49a0a568b6dd2ea31fdf946
Hakari Bibani <hakary.jalal33@gmail.com>
2024-11-08 16:12:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 # Set page configuration
 st.set_page_config(page_title="Acid-Base Titration")
@@ -30,56 +31,31 @@ This experiment demonstrates the titration of Hydrochloric Acid (HCl) with Sodiu
 """)
 
 # Add burette and beaker
-st.write("""
-<style>
-  .burette, .beaker {
-    width: 100px;
-    height: 300px;
-    position: relative;
-    display: inline-block;
-    margin: 20px;
-  }
+burette_height = 300
+beaker_height = 200
+burette_liquid_height = 0
+beaker_liquid_height = 0
 
-  .burette-liquid, .beaker-liquid {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    background-color: #ccc;
-    transition: height 1s;
-  }
-
-  .burette-liquid {
-    height: 0%;
-  }
-
-  .beaker-liquid {
-    height: 0%;
-    background-color: #fff;
-  }
-</style>
-
-<div class="burette">
-  <div class="burette-liquid"></div>
-</div>
-<div class="beaker">
-  <div class="beaker-liquid"></div>
-</div>
-""", unsafe_allow_html=True)
+burette = st.empty()
+beaker = st.empty()
 
 # Add start experiment button
 if st.button("Start Experiment"):
     # Animate burette and beaker
-    beaker_liquid = st.markdown("""
-    <script>
-      const beakerLiquid = document.querySelector('.beaker-liquid');
-      beakerLiquid.style.height = '50%';
-      beakerLiquid.style.backgroundColor = '#ff0000';
-
-      const buretteLiquid = document.querySelector('.burette-liquid');
-      buretteLiquid.style.height = '20%';
-    </script>
-    """, unsafe_allow_html=True)
+    for i in range(1, 21):
+        burette_liquid_height = i * (burette_height / 20)
+        beaker_liquid_height = i * (beaker_height / 20)
+        burette.markdown(f"""
+        <div class="burette" style="height:{burette_height}px;width:100px;">
+          <div class="burette-liquid" style="height:{burette_liquid_height}px;background-color:#ccc;"></div>
+        </div>
+        """, unsafe_allow_html=True)
+        beaker.markdown(f"""
+        <div class="beaker" style="height:{beaker_height}px;width:100px;">
+          <div class="beaker-liquid" style="height:{beaker_liquid_height}px;background-color:#ff0000;"></div>
+        </div>
+        """, unsafe_allow_html=True)
+        time.sleep(0.1)
 
     # Plot pH curve
     x = np.linspace(0, 14, 100)
@@ -91,6 +67,8 @@ if st.button("Start Experiment"):
     ax.set_ylabel('pH')
     ax.set_title('pH Curve')
     ax.axvline(x=7.1, color='r', linestyle='--', label='Equivalence Point')
+    ax.set_xlim(0, 14)
+    ax.set_ylim(0, 14)
     ax.legend()
 
     st.pyplot(fig)
